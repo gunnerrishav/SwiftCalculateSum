13ad0abf3320961f2c38234b137331e8fcb6f983
Rishab Maharjan <gunnerrishav@gmail.com>
2024-07-03 16:02:07 +0545

    Add calculateSum function

---
+++ SwiftCalculateSum/Module/CalculateSum.swift
@@ -0,0 +1,22 @@
+//
+//  CalculateSum.swift
+//  SwiftCalculateSum
+//
+//  Created by Rishab Maharjan on 03/07/2024.
+//
+
+import Foundation
+
+public class CalculateSum {
+    /// Initializes the SDK.
+    public init() {}
+    
+    /// Performs a sample action.
+    public func performAction() {
+        print("Action performed")
+    }
+    
+    func calcuateSum(of num1: Double, and num2: Double) -> Double{
+        return num1 + num2
+    }
+}
