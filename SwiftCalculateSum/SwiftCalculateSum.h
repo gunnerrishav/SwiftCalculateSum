762f92d0f3c89426c39706327ceb3695017dd777
Rishab Maharjan <gunnerrishav@gmail.com>
2024-07-03 15:44:47 +0545

    Initial Commit

---
+++ SwiftCalculateSum/SwiftCalculateSum.h
@@ -0,0 +1,18 @@
+//
+//  SwiftCalculateSum.h
+//  SwiftCalculateSum
+//
+//  Created by Rishab Maharjan on 03/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCalculateSum.
+FOUNDATION_EXPORT double SwiftCalculateSumVersionNumber;
+
+//! Project version string for SwiftCalculateSum.
+FOUNDATION_EXPORT const unsigned char SwiftCalculateSumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCalculateSum/PublicHeader.h>
+
+
